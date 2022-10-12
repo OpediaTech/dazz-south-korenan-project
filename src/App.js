cd836359f728257960e88cdee3a8dfd5f9c6f8c9
opppdia <hannan.opppdia@gmail.com>
2022-10-12 14:24:41 +0600

    new boilarplate

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Md Nahid Murad Abir</p>
+        <a
+          className="App-link"
+          href="https://nahid-murad-abir.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Profile || Github
+        </a>
+        <p>Hello there hush again sdfd</p>
+      </header>
+
+      <p>Hosting Conneccted updated</p>
+    </div>
+  );
+}
+
+export default App;
